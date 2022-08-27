d2bd74613bb160294a1de8deb1811a324243ec7e
Sankalp Sethi <54805355+YourSam07@users.noreply.github.com>
2022-08-27 09:30:47 +0530

    final

--- src/components/Certificate.jsx
+++ src/components/Certificate.jsx
@@ -1,5 +1,8 @@
-import React, { createRef } from 'react'
+import React, { createRef, useContext } from 'react'
 import PDF from 'react-to-pdf'
+import { UserContext } from '../Contexts/UserContext'
+import sign from "../Assets/sign1.png"
+import sign2 from "../Assets/sign2.png"
 
 const ref = createRef()
 const options = {
@@ -9,6 +12,7 @@ const options = {
 }
 
 const Certificate = () => {
+  const [{currentUser}] = useContext(UserContext)
   return (
     <div className="pageWrapper flex flex-col items-center justify-center min-h-screen">
       <div className="certificate mb-8" ref={ref}>
@@ -21,7 +25,27 @@ const Certificate = () => {
                     <div className="head text-4xl font-bold text-orange-400 text-center my-8">
                       NeuroLingua Course Completion Certificate
                     </div>
-                    <div className="text-lg text-black "></div>
+                    <div className="para">
+                      <div className="text-xl italic text-black px-20 pt-12">This is to certify <span className='text-2xl font-bold underline not-italic'>{currentUser.name}</span> for the successful completion of the course</div>
+                      <div className='px-20 text-3xl font-bold text-center py-8'>"{currentUser.lang} - {currentUser.course}"</div>
+                    </div>
+                    <div className="signs flex justify-between items-center mt-20">
+                      <div className="left flex-1 flex-col px-8">
+                        <div className="img">
+                          <img src={sign} alt="" className="h-[75px] w-[200px]"/>
+                        </div>
+                        <div className="line h-[2px] w-1/3 mt-2 bg-black"></div>
+                        <div className="text-lg">Instructor's Signature</div>
+                      </div>
+                      <div className="left flex-2 flex-col px-8 ">
+                        <div className="img">
+                          <img src={sign2} alt="" className="h-[75px] w-[200px]"/>
+                        </div>
+                        <div className="line h-[2px] w-full mt-2 bg-black"></div>
+                        <div className="text-lg">Head's Signature</div>
+                      </div>
+                    </div>
+                  
                   </div>
                 </div>
               </div>
